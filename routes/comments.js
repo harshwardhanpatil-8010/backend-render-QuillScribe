bae576222614a32ab3ac21c8764b44f55e0d20ab
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-04-27 11:24:24 +0530

    new

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import Comment from '../../models/Comment.js';
-import authMiddleware from '../../middlewares/authMiddleware.js';
+import Comment from '../models/Comment.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
