0af3d80c8bcdfd22ff27fe0171821274ded1a800
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-04-27 20:17:12 +0530

    new

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,20 @@
 import mongoose from 'mongoose';
 
 const commentSchema = new mongoose.Schema({
-    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    text: { type: String, required: true },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true });
 
 const Comment = mongoose.model('Comment', commentSchema);
